07fa3f33dfb3e0917cfdf02529497090c4b9e8c2
KelzangUD <kuding755@gmail.com>
2025-05-28 09:19:26 +0600

    hero api U

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -407,6 +407,7 @@ export interface ApiBlogBlog extends Struct.CollectionTypeSchema {
 export interface ApiHeroHero extends Struct.CollectionTypeSchema {
   collectionName: 'heroes';
   info: {
+    description: '';
     displayName: 'Hero';
     pluralName: 'heroes';
     singularName: 'hero';
@@ -423,7 +424,7 @@ export interface ApiHeroHero extends Struct.CollectionTypeSchema {
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::hero.hero'> &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
-    subheadr: Schema.Attribute.Text;
+    subheader: Schema.Attribute.Text;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
