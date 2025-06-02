6bdb1dbc7174a9b0d339a440e14cdd8e2db1580e
KelzangUD <kuding755@gmail.com>
2025-06-02 10:04:25 +0600

    cros origin issue update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,23 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
-      origin: ['https://portfolio-kelzanguds-projects.vercel.app'], // allow Vercel app
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
+      origin: [
+        "https://portfolio-kelzanguds-projects.vercel.app",
+        "http://localhost:3000",
+      ], // allow Vercel app
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
       credentials: true,
     },
   },
-  'strapi::security',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::security",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
